5184c7c74921a249c37d3e494a2e51b72698a81b
PALASH2201 <palashmahajan22@gmail.com>
2026-01-02 16:55:37 +0530

    added postgres db to docker-compose.yml

---
+++ init/01-create-databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "projectService";
+CREATE DATABASE "notificationService";
+
+GRANT ALL PRIVILEGES ON DATABASE "projectService" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "notificationService" TO postgres;
